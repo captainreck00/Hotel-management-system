197db691bcc1768a2e7c89cf96e0b1469626a964
captainreck00 <117888395+captainreck00@users.noreply.github.com>
2023-08-30 09:58:45 +0000

    Do it

--- Web/script.js
+++ Web/script.js
@@ -1,17 +1,22 @@
-const buttons = document.querySelectorAll("[data-carousel-btn]");
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        const offset = button.dataset.carouselBtn === "next" ? 1 : -1;
-        const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
 
-        const activeSlide = slides.querySelector("[data-active]");
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll("[data-carousel-btn]");
 
-        if (newIndex < 0) newIndex = slides.children.length - 1;
-        if (newIndex >= slides.children.length) newIndex = 0;
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const offset = button.dataset.carouselBtn === "next" ? 1 : -1;
+            const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
 
-        activeSlide.removeAttribute("data-active");
-        slides.children[newIndex].setAttribute("data-active", "");
-    });
+            const activeSlide = slides.querySelector("[data-active]");
+            let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+
+            if (newIndex < 0) newIndex = slides.children.length - 1;
+            if (newIndex >= slides.children.length) newIndex = 0;
+
+            activeSlide.removeAttribute("data-active");
+            slides.children[newIndex].setAttribute("data-active", "");
+        });
+});    
 });
+  
